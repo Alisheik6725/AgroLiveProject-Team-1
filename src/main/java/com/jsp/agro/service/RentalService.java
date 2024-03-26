f5187669837a9f52ad2ea4ef1eddb4a577fc2cfb
Surya-Prakash045 <118025920+Surya-Prakash045@users.noreply.github.com>
2024-03-26 17:08:39 +0530

    rental service from Surya

--- src/main/java/com/jsp/agro/service/RentalService.java
+++ src/main/java/com/jsp/agro/service/RentalService.java
@@ -1,78 +1,54 @@
-package com.jsp.agro.service;
+package com.jsp.agroLiveProject.service;
 
-import java.time.LocalDateTime;
-import java.time.Period;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.temporal.ChronoField;
+import java.time.Duration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.jsp.agro.dao.EquipmentDao;
-import com.jsp.agro.dao.PaymentHistoryDao;
-import com.jsp.agro.dao.RentalDao;
-import com.jsp.agro.entity.Equipment;
-import com.jsp.agro.entity.PaymentHistory;
-import com.jsp.agro.entity.Rental;
-import com.jsp.agro.exception.EquipmentNotFound;
-import com.jsp.agro.util.ResponseStructure;
-import com.jsp.agro.util.TimeCalculator;
+import com.jsp.agroLiveProject.dao.EquipmentDao;
+import com.jsp.agroLiveProject.dao.RentalDao;
+import com.jsp.agroLiveProject.entity.Equipment;
+import com.jsp.agroLiveProject.entity.PaymentHistory;
+import com.jsp.agroLiveProject.entity.Rental;
+import com.jsp.agroLiveProject.util.ResponseStructure;
 
 @Service
 public class RentalService {
+
 	@Autowired
-	private RentalDao rentalDao;
-	@Autowired
-	private Rental rental;
-	@Autowired
-	private EquipmentDao equipmentDao;
-	@Autowired
-	private PaymentHistory payment;
-	@Autowired
-	private PaymentHistoryDao paymentDao;
+	private RentalDao rDao;
 	
-	
-	DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-	        .appendPattern("yyyy-MM-dd[ HH:mm:ss]")
-	        .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
-	        .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
-	        .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
-	        .toFormatter();
-	
-	
-	public ResponseEntity<ResponseStructure<Rental>> saveRental(String equipmentId,String startDateAndTime,String endDateAndTime){
-		Equipment equipmentDb = equipmentDao.fetchEquipmentById(equipmentId);
-		if(equipmentDb!=null) {
-			rental.setEquipment(equipmentDb);
-			LocalDateTime fromDateTime= LocalDateTime.parse(startDateAndTime,formatter);
-	    	LocalDateTime toDateTime= LocalDateTime.parse(endDateAndTime,formatter);
-	    	rental.setStartDataAndTime(fromDateTime);
-	    	rental.setEndDataAndTime(toDateTime);
-			long[] duration = TimeCalculator.getDuration(fromDateTime, toDateTime);
-			double amount=((duration[0]*365)*24+(duration[1]*30)*24+duration[2]*24+duration[3])*equipmentDb.getCostPerHrs();
-			System.out.println("year "+(duration[0]*365)*24);
-			System.out.println("month "+(duration[1]*30)*24);
-			System.out.println("day "+duration[2]*24);
-			System.out.println("hours "+duration[3]);
-				
-			payment.setAmount(amount);
-			PaymentHistory paymentDb = paymentDao.savePayment(payment);
-			rental.setPaymentHistory(paymentDb);
+	@Autowired
+	private EquipmentDao eDao;
+
+	public ResponseEntity<ResponseStructure<Rental>> saveRental(int eid, Rental r) {
+		Equipment edb = eDao.fetchEquipmentById(eid);
+		if(edb!=null) {
+			r.setEquipment(edb);
+			Duration time = Duration.between(r.getStartTime(), r.getEndTime());
+			double days=time.toDaysPart();
+			double hrs=time.toHoursPart();
+			double min=time.toMinutesPart();
+			double totaltime=days*24+hrs+min/60;
+			double amount=totaltime*edb.getCostPerHr();
+			System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+			System.out.println(time);
+			System.out.println(days+""+hrs+""+min+""+totaltime+":"+amount);
+			PaymentHistory pay=new PaymentHistory();
+			pay.setAmount(amount);
+			r.setPaymentHistory(pay);
+			Rental rdb = rDao.saveRental(r);
 			
-			Rental rentalDb = rentalDao.saveRental(rental);
 			
-			ResponseStructure<Rental> response=new ResponseStructure<Rental>();
-			response.setData(rentalDb);
-			response.setMsg("saved successfully........!");
-			response.setStatus(HttpStatus.CONTINUE.value());
-			return new ResponseEntity<ResponseStructure<Rental>>(response,HttpStatus.CONTINUE);
+			ResponseStructure<Rental> rs=new ResponseStructure<Rental>();
+			rs.setData(rdb);
+			rs.setMsg("Rental saved succeesfully");
+			rs.setStatus(HttpStatus.CREATED.value());
+			return new ResponseEntity<ResponseStructure<Rental>>(rs,HttpStatus.CREATED);
 			
 		}
-		throw new EquipmentNotFound("equipment not found for id : "+equipmentId);
-		
+		return null;
 	}
-
 }
